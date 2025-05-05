52f3a6d73779714e1aefe0779d0c70c8046315ba
J. Cameron McDonald <cameronhhimself@gmail.com>
2025-05-05 16:16:58 -0500

    test: switch from jest to mocha/chai

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,3 +1,5 @@
+import "mocha";
+import { assert } from "chai";
 import { NUL, SOH, STX } from "./constants";
 import {
   bufferFromMixed,
@@ -12,89 +14,67 @@ import {
 import { Shortcuts } from "./classes";
 
 describe("parseNumberBuffer", () => {
-  const num = 2515600387;
-  const numBuffer = Buffer.alloc(4);
-  numBuffer.writeUInt32LE(num);
-  const buf = bufferFromMixed(STX, "numericKey", NUL, numBuffer);
-  const [key, value] = parseNumberBuffer(buf);
-  expect(key).toEqual("numericKey");
-  expect(value).toEqual(num);
+  it("parses a number from a buffer", () => {
+    const num = 2515600387;
+    const numBuffer = Buffer.alloc(4);
+    numBuffer.writeUInt32LE(num);
+    const buf = bufferFromMixed(STX, "numericKey", NUL, numBuffer);
+    const [key, value] = parseNumberBuffer(buf);
+    assert.strictEqual(key, "numericKey");
+    assert.strictEqual(value, num);
+  });
 });
 
 describe("parseStringBuffer", () => {
-  const buf = bufferFromMixed(SOH, "someKey", NUL, "some value", NUL);
-  const [key, value] = parseStringBuffer(buf);
-  expect(key).toEqual("someKey");
-  expect(value).toEqual("some value");
+  it("parses a string from a buffer", () => {
+    const buf = bufferFromMixed(SOH, "someKey", NUL, "some value", NUL);
+    const [key, value] = parseStringBuffer(buf);
+    assert.strictEqual(key, "someKey");
+    assert.strictEqual(value, "some value");
+  });
 });
 
 describe("parseStringArrayBuffer", () => {
   it("parses an empty array", () => {
     const buf = bufferFromMixed(NUL, "emptyTags", NUL);
     const [k, value] = parseStringArrayBuffer(buf);
-    expect(k).toEqual("emptyTags");
-    expect(value).toEqual([]);
+    assert.strictEqual(k, "emptyTags");
+    assert.deepEqual(value, []);
   });
 
-  const buf = bufferFromMixed(
-    NUL, "tags", NUL,
-      SOH, "0", NUL, "some tag value", NUL,
-      SOH, "1", NUL, "Another tag", NUL,
-  );
-  const [k, value] = parseStringArrayBuffer(buf);
-  expect(k).toEqual("tags");
-  expect(value.length).toEqual(2);
-  expect(value[0]).toEqual("some tag value");
-  expect(value[1]).toEqual("Another tag");
+  it("parses an array of strings", () => {
+    const buf = bufferFromMixed(
+      NUL, "tags", NUL,
+        SOH, "0", NUL, "some tag value", NUL,
+        SOH, "1", NUL, "Another tag", NUL,
+    );
+    const [k, value] = parseStringArrayBuffer(buf);
+    assert.strictEqual(k, "tags");
+    assert.lengthOf(value, 2);
+    assert.strictEqual(value[0], "some tag value");
+    assert.strictEqual(value[1], "Another tag");
+  });
 });
-
-// describe("getGenericFieldBuffer", () => {
-//   const mock = bufferFromMixed(
-//     NUL, "shortcuts", NUL,
-//     NUL, "0", NUL,
-//     STX, "appid",        NUL, 0x43, 0xA4, 0xF3, 0x19,
-//     SOH, "AppName",      NUL, "App name goes here", NUL,
-//     SOH, "StartDir",     NUL, '"Directory to start in"', NUL,
-//     SOH, "icon",         NUL, '"Path to icon"', NUL,
-//     SOH, "ShortcutPath", NUL, "Shortcut path here", NUL,
-//     STX, "LastPlayTime", NUL, 0x68, 0x10, 0x36, 0xAA,
-//     NUL, "tags",         NUL,
-//       SOH, "0", NUL, "some tag", NUL,
-//       SOH, "1", NUL, "a second tag", NUL,
-//   );
-//   const appid = getGenericFieldBuffer(mock, "appid");
-//   expect(appid.length).toEqual(11);
-//   expect(appid[0]).toEqual(STX);
-//   expect(appid[appid.length - 4]).toEqual(0x43);
-//   expect(appid[appid.length - 1]).toEqual(0x19);
-
-//   const lastPlayTime = getFieldBuffer(mock, "LastPlayTime");
-//   expect(lastPlayTime.length).toEqual(18);
-//   expect(lastPlayTime[0]).toEqual(STX);
-//   expect(lastPlayTime[lastPlayTime.length - 4]).toEqual(0x68);
-//   expect(lastPlayTime[lastPlayTime.length - 1]).toEqual(0xAA);
-
-// });
 
 describe("bufferFromMixed", () => {
   it("converts single string into matching Buffer", () => {
     const buf = bufferFromMixed("foobar");
-    expect(buf).toBeInstanceOf(Buffer);
-    expect(buf.toString()).toEqual("foobar");
+    assert.instanceOf(buf, Buffer);
+    assert.strictEqual(buf.toString(), "foobar");
   });
   it("converts numbers into matching Buffer", () => {
     const buf = bufferFromMixed(0, 0x09, 0x07);
-    expect(buf[0]).toEqual(0);
-    expect(buf[1]).toEqual(0x09);
-    expect(buf[2]).toEqual(0x07);
+    assert.strictEqual(buf[0], 0);
+    assert.strictEqual(buf[1], 0x09);
+    assert.strictEqual(buf[2], 0x07);
   });
   it("converts mixed numbers and strings into matching Buffer", () => {
     const buf = bufferFromMixed(0x43, "oo", 0x6C);
-    expect(buf.toString()).toEqual("Cool");
+    assert.strictEqual(buf.toString(), "Cool");
   });
   it("converts mixed numbers, strings, and buffers into matching Buffer", () => {
     const buf = bufferFromMixed(0x43, "oo", 0x6C, Buffer.from("!!!"));
-    expect(buf.toString()).toEqual("Cool!!!");
+    assert.strictEqual(buf.toString(), "Cool!!!");
   });
 });
 
@@ -102,22 +82,22 @@ describe("findIndexOfBufferInBuffer", () => {
   it("returns correct index of buffer", () => {
     const haystack = Buffer.from([0x0, 0x5, 0x18, 0xFF, 0xC9]);
     const needle = Buffer.from([0x18, 0xFF]);
-    expect(findIndexOfBufferInBuffer(haystack, needle)).toEqual(2);
+    assert.strictEqual(findIndexOfBufferInBuffer(haystack, needle), 2);
   });
   it("returns -1 on failed search", () => {
     const haystack = Buffer.from([0x0, 0x5, 0x18, 0xFF, 0xC9]);
     const needle = Buffer.from([0x18, 0xFA]);
-    expect(findIndexOfBufferInBuffer(haystack, needle)).toEqual(-1);
+    assert.strictEqual(findIndexOfBufferInBuffer(haystack, needle), -1);
   });
   it("returns index of first match with multiple matches", () => {
     const haystack = Buffer.from([0x0, 0x5, 0x18, 0xFF, 0xC9, 0x18, 0xFF]);
     const needle = Buffer.from([0x18, 0xFF]);
-    expect(findIndexOfBufferInBuffer(haystack, needle)).toEqual(2);
+    assert.strictEqual(findIndexOfBufferInBuffer(haystack, needle), 2);
   });
 });
 
 describe("getFieldBuffer", () => {
-  it("works", () => {
+  it("extracts the expected field buffers", () => {
     const mock = bufferFromMixed(
       NUL, "shortcuts", NUL,
       NUL, "0", NUL,
@@ -132,22 +112,23 @@ describe("getFieldBuffer", () => {
         SOH, "0", NUL, "some tag", NUL,
         SOH, "1", NUL, "a second tag", NUL,
     );
+
     const appid = getFieldBuffer(mock, "appid");
-    expect(appid.length).toEqual(11);
-    expect(appid[0]).toEqual(STX);
-    expect(appid[appid.length - 4]).toEqual(0x43);
-    expect(appid[appid.length - 1]).toEqual(0x19);
+    assert.strictEqual(appid.length, 11);
+    assert.strictEqual(appid[0], STX);
+    assert.strictEqual(appid[appid.length - 4], 0x43);
+    assert.strictEqual(appid[appid.length - 1], 0x19);
 
     const lastPlayTime = getFieldBuffer(mock, "LastPlayTime");
-    expect(lastPlayTime.length).toEqual(18);
-    expect(lastPlayTime[0]).toEqual(STX);
-    expect(lastPlayTime[lastPlayTime.length - 4]).toEqual(0x68);
-    expect(lastPlayTime[lastPlayTime.length - 1]).toEqual(0xAA);
+    assert.strictEqual(lastPlayTime.length, 18);
+    assert.strictEqual(lastPlayTime[0], STX);
+    assert.strictEqual(lastPlayTime[lastPlayTime.length - 4], 0x68);
+    assert.strictEqual(lastPlayTime[lastPlayTime.length - 1], 0xAA);
 
     const tags = getFieldBuffer(mock, "tags");
-    expect(tags.length).toEqual(34);
-    expect(tags[0]).toEqual(NUL);
-    expect(tags.subarray(1, 5).toString()).toEqual("tags");
-    expect(tags.subarray(tags.length - 1)[0]).toEqual(NUL);
+    assert.strictEqual(tags.length, 34);
+    assert.strictEqual(tags[0], NUL);
+    assert.strictEqual(tags.subarray(1, 5).toString(), "tags");
+    assert.strictEqual(tags.subarray(tags.length - 1)[0], NUL);
   });
 });
