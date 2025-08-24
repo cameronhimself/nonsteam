d790438f74e60ea97c2029dc3200a0073e7b98d3
J. Cameron McDonald <cameronhhimself@gmail.com>
2025-08-24 10:48:26 -0500

    chore: fix types and exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 export * from "./classes";
 export * from "./constants";
 export * from "./utils";
+export * from "./types";
 
 import { NonSteam } from "./classes";
 
 export const load = async (path?: string): Promise<NonSteam> =>
   NonSteam.load(path);
+export { NonSteam };
